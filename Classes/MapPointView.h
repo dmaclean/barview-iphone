f7ef4cccd2b97ab7d7a0330bc0e95621ac7a1814
Dan MacLean <dmaclean82@gmail.com>
2011-07-08 23:58:46 -0400

    Removed unnecessary files that were leftover from the Big Nerd Ranch exercises.  Also added a custom MapPointView class that isn't being used yet.

---
+++ Classes/MapPointView.h
@@ -0,0 +1,16 @@
+//
+//  MapPointView.h
+//  Barview
+//
+//  Created by Dan MacLean on 6/24/11.
+//  Copyright 2011 UMass Dartmouth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MapPointView : NSObject {
+    
+}
+
+@end
---
+++ Classes/MapPointView.m
@@ -0,0 +1,14 @@
+//
+//  MapPointView.m
+//  Barview
+//
+//  Created by Dan MacLean on 6/24/11.
+//  Copyright 2011 UMass Dartmouth. All rights reserved.
+//
+
+#import "MapPointView.h"
+
+
+@implementation MapPointView
+
+@end
